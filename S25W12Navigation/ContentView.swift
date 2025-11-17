f78123b410c0f4f7830fb54fa907e1bcd05ab591
parkjunhyung <pjh6395673066@gmail.com>
2025-11-17 16:48:17 +0900

    🏗️path 사용

--- S25W12Navigation/ContentView.swift
+++ S25W12Navigation/ContentView.swift
@@ -1,26 +1,71 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var viewModel = SongViewModel()
+    
     var body: some View {
-        NavigationStack {
-            Text("노래 화면")
-
-            NavigationLink(destination: SingerView()) {
-                Text("가수 화면으로 이동")
+        NavigationStack(path: $viewModel.path) {
+            List(viewModel.songs) { song in
+//                NavigationLink(destination: SongDetailView(song: song)) {
+                NavigationLink(value: song) {
+                    HStack {
+                        VStack(alignment: .leading) {
+                            Text(song.title)
+                                .font(.headline)
+                            Text(song.singer)
+                                .font(.subheadline)
+                                .foregroundColor(.gray)
+                        }
+                    }
+                }
+            }
+            .navigationDestination(for: Song.self) { song in
+                SongDetailView(song: song)
             }
         }
     }
 }
 
-struct SingerView: View {
-    var body: some View {
-        NavigationStack {
-            Text("가수 화면")
+//struct SingerView: View {
+//    var body: some View {
+//        NavigationStack {
+//            Text("가수 화면")
+//
+////            NavigationLink(destination: SingerView()) {
+////                Text("노래 화면으로 이동")
+////            }
+//        }
+//    }
+//}
 
-//            NavigationLink(destination: SingerView()) {
-//                Text("노래 화면으로 이동")
-//            }
+struct SongDetailView: View {
+    let song: Song
+
+    var body: some View {
+        ScrollView {
+            VStack(alignment: .leading, spacing: 15) {
+                HStack {
+                    Text(song.singer)
+                        .font(.title2)
+                        .foregroundColor(.secondary)
+                    Spacer()
+                    Text(String(song.rating))
+                        .font(.title)
+                        .foregroundColor(.yellow)
+                }
+                .padding(.bottom, 10)
+
+                Text(song.lyrics)
+                    .font(.body)
+                    .multilineTextAlignment(.leading)
+            }
+            .padding()
         }
+        .navigationTitle(song.title)
+        #if os(iOS)
+        .navigationBarTitleDisplayMode(.large)
+        #endif
+
     }
 }
 
